c52ac1ea490515b8c17257cf74b5acaac1ebf1f6
eugeneLuck <eugeneLuck@gmail.com>
2023-07-02 11:03:45 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace wpfClock_06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.IsEnabled = true;
+            timer.Tick += (s, e) => time.Text = DateTime.Now.ToLongTimeString();
+
+            MouseLeftButtonDown += (s, e) => DragMove();
+            minButton.Click += (s, e) => WindowState = WindowState.Minimized;
+            closeButton.Click += (s, e) => Close();
+        }
+    }
+}
